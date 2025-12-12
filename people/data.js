f71260da4e1627a85d32eae6f560d5e5a084a3d6
Tino Schuldt <tinoschuldt100@gmail.com>
2025-12-12 23:31:04 +0100

    update layout and add people page

---
+++ people/data.js
@@ -0,0 +1,12 @@
+const people = [
+  {
+    name: "Werner von Braun",
+    life: "1912–1977",
+    image: "images/braun.jpg"
+  },
+  {
+    name: "Muchio Kaku",
+    life: "1947–....",
+    image: "images/kaku.jpg"
+  },
+];
