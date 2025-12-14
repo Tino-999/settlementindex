cfdfa43f8c751221ff70ad7ff83dc2800ada3bf3
Tino-999 <tinoschuldt100@gmail.com>
2025-12-14 13:41:40 +0100

    build: emit movies/data.js

--- movies/data.js
+++ movies/data.js
@@ -1,16 +1,19 @@
-// movies/data.js
-const people = [
+export const MOVIES = [
   {
-    name: "Passengers",
-    life: "2016",
-    image: "images/passengers.avif",
-    wiki: "https://en.wikipedia.org/wiki/Passengers_(2016_film)",
-
-    book: "Passengers",
-    bookYear: "1969",
-    bookWiki: "https://en.wikipedia.org/wiki/Passengers_(novel)",
-
-    authorName: "Robert Silverberg",
-    authorSlug: "robert-silverberg"
+    "slug": "the-martian",
+    "title": "The Martian",
+    "year": 2015,
+    "format": "Film",
+    "summary": "Ein Astronaut kämpft ums Überleben auf dem Mars.",
+    "people": [],
+    "tags": [
+      "mars"
+    ],
+    "links": [
+      {
+        "label": "IMDb",
+        "url": "https://www.imdb.com/title/tt3659388/"
+      }
+    ]
   }
-];
+]
